9e204c29082e942908271977ec2663656ca6c3fd
Thaís Monteiro <77128802+thaxz@users.noreply.github.com>
2023-10-01 16:04:17 -0300

    feature(SceneConfiguration): Creating a enum that will hold every possible action in our scene

---
+++ FirstARProject/Enums/ARAction.swift
@@ -0,0 +1,17 @@
+//
+//  ARAction.swift
+//  FirstARProject
+//
+//  Created by thaxz on 01/10/23.
+//
+
+import SwiftUI
+
+// Enum with all the options that we're going to have on our ar scene
+// Everything that we'll be able to do on our scene will have a case in this enum
+enum ARAction {
+    
+    case placeBlock(color: Color)
+    case removeAllAnchors
+    
+}
