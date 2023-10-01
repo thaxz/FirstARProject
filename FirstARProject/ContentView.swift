d403ea497c282998d9bfd905646080bff3082499
Thaís Monteiro <77128802+thaxz@users.noreply.github.com>
2023-10-01 16:19:53 -0300

    feature(SceneConfiguration): Adding mock ui and connecting with passthroughObject

--- FirstARProject/ContentView.swift
+++ FirstARProject/ContentView.swift
@@ -8,11 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // colors that the user can choose
+    @State private var colors: [Color] = [.green, .red, .blue]
+    
     var body: some View {
         CustomARViewRepresentable()
             .ignoresSafeArea()
+            .overlay(alignment: .bottom){
+                ScrollView(.horizontal){
+                    HStack{
+                        Button {
+                            // Passing remove action by combine
+                            ARManager.shared.actionsStream.send(.removeAllAnchors)
+                        } label: {
+                            Image(systemName: "trash")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 40, height: 40)
+                                .padding()
+                                .background(.regularMaterial)
+                                .cornerRadius(16)
+                        }
+                        ForEach(colors, id: \.self){ color in
+                            Button {
+                                // Passing place block action by combine
+                                ARManager.shared.actionsStream.send(.placeBlock(color: color))
+                            } label: {
+                                color
+                                    .frame(width: 40, height: 40)
+                                    .padding()
+                                    .background(.regularMaterial)
+                                    .cornerRadius(16)
+                            }
+                        }
+                    }
+                    .padding()
+                }
+            }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
