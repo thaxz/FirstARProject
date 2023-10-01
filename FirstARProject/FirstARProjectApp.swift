a9592f72071297ee8789731d70de618a15c471f3
Thaís Monteiro <77128802+thaxz@users.noreply.github.com>
2023-10-01 15:00:49 -0300

    first commit

---
+++ FirstARProject/FirstARProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstARProjectApp.swift
+//  FirstARProject
+//
+//  Created by thaxz on 01/10/23.
+//
+
+import SwiftUI
+
+@main
+struct FirstARProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
