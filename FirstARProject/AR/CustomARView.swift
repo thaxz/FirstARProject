84ff5283a8fa1969141ca87c1a1fe17b2a3d4150
Thaís Monteiro <77128802+thaxz@users.noreply.github.com>
2023-10-01 15:29:24 -0300

    feature(SceneConfiguration): Adding default configuration: objects are now placed relatively to iPhone's coordinates

--- FirstARProject/AR/CustomARView.swift
+++ FirstARProject/AR/CustomARView.swift
@@ -11,6 +11,8 @@ import SwiftUI
 
 class CustomARView: ARView {
     
+    // Required inits
+    
     required init(frame frameRect: CGRect) {
         super.init(frame: frameRect)
     }
@@ -21,6 +23,15 @@ class CustomARView: ARView {
     
     convenience init(){
         self.init(frame: UIScreen.main.bounds)
+    }
+    
+    // Configuration
+    
+    func configurationExamples(){
+        // Tracks the device relative to it's environment
+        // Needed to place content that is relative to your phone
+        let configuration = ARWorldTrackingConfiguration()
+        session.run(configuration)
     }
     
     
